378c8909eb0d10ab259098184ca2e71cf06d7aac
Brendan Dugan <brendandugan@MacBook-Pro-3.local>
2017-04-08 13:13:11 -0700

    Add buttons to nav

--- cow-client/src/pages/home/home.ts
+++ cow-client/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// TODO: replace with wizard
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,14 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  navToFarmer(){
+    // TODO: farm wizard
+    this.navCtrl.push(ListPage)
+  }
+  navToExporter(){
+    // TODO: switch to exporter wizard page
+    this.navCtrl.push(ListPage)
+  }
+
 
 }
